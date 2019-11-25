f29df55a77303089b7842d461678150d23e9a4a2
velocity <sagaryc111@gmail.com>
2019-11-25 13:39:08 +0530

    table functionality to be done

--- additem.php
+++ additem.php
@@ -11,7 +11,6 @@ $rate = $_POST['rate'];
 
 
 
-if(isset($_POST['submit'])){
 
 $query = " INSERT INTO additem (category_id, item_name, quantity, vendor, rate) VALUES ('$category_id','$item_name','$quantity','$vendor','$rate') ";
 mysqli_query($conn, $query);
